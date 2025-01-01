dbf04b45165b56fbdfc2d57a09cbc9a8b30b6219
MohsinAwan214 <mohsinawan2267@gmai.com>
2025-01-01 17:52:37 +0500

    Add Changes

--- src/Component/Music.jsx
+++ src/Component/Music.jsx
@@ -4,30 +4,30 @@ import Radio from "../assets/images/Frame 694.png"
 
 export default function Music() {
   return (
-    <div className="bg-[#000000] h-[400px] text-white flex music ">
-<div className="ml-12 music-1 h-full  max-w-96 my-12  flex flex-col border justify-between ">
-  <div className="text-[#00FF66] leading-5 musicText">Categories</div>
-  <div className=" text-5xl w-96 musicText-1 border">Enhance Your Music Experience</div>
-  <div className='flex text-black gap-5 border music-2'>
+    <div className="bg-[#000000] h-[400px] text-white flex  ">
+<div className="ml-12  h-full  max-w-96  flex flex-col border justify-between py-10 ">
+  <div className="text-[#00FF66] leading-5 ">Categories</div>
+  <div className=" text-5xl w-96  border">Enhance Your Music Experience</div>
+  <div className='flex text-black gap-5 border '>
 
-   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 text-center  flex-col days">
-      <li className='text-sm font-bold text'>23</li>
-      <li className='text-[8px] text-center text-1'>Days</li>
+   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 text-center  flex-col ">
+      <li className='text-sm font-bold '>23</li>
+      <li className='text-[8px] text-center '>Days</li>
    </ul>
    
-   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 text-center   hours">
-      <li className='text-sm font-bold text'>05</li>
-      <li className='text-[8px]  text-center text-1'>Hours</li>
+   <ul className="bg-white h-12 w-12 rounded-full z-10 px-2 text-center   ">
+      <li className='text-sm font-bold '>05</li>
+      <li className='text-[8px]  text-center '>Hours</li>
    </ul>
    
-   <ul className="bg-white h-12 w-12  rounded-full z-10  text-center   minutes">
-      <li className='text-sm font-bold text'>59</li>
-      <li className='text-[8px]  text-center text-1'>Minutes</li>
+   <ul className="bg-white h-12 w-12  rounded-full z-10 flex-col  bg-center   ">
+      <li className='text-sm font-bold '>59</li>
+      <li className='text-[8px]  text-center '>Minutes</li>
    </ul> 
    
-   <ul className="bg-white h-12 w-12 rounded-full z-10  text-center  seconds">
+   <ul className="bg-white h-12 w-12 rounded-full z-10  text-center  ">
       <li className='text-sm text-center font-bold text'>35</li>
-      <li className='text-[8px] text-center text-1'>Seconds</li>
+      <li className='text-[8px] text-center '>Seconds</li>
    </ul>
    </div>
  <div className="bg-[#00FF66] h-14 w-[171px] BayNow-btn
